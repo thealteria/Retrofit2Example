81ae03ea302c95b55495e3580794c6ae927bae75
Aman Gupta <sarcasaman@gmail.com>
2019-06-22 17:58:59 +0530

    migrated to androidx

--- app/src/main/java/com/example/retrofit2example/HeroRepository.java
+++ app/src/main/java/com/example/retrofit2example/HeroRepository.java
@@ -1,8 +1,8 @@
 package com.example.retrofit2example;
 
 import android.app.Application;
-import android.arch.lifecycle.MutableLiveData;
-import android.support.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.annotation.NonNull;
 
 import java.util.List;
 
--- app/src/main/java/com/example/retrofit2example/MainActivity.java
+++ app/src/main/java/com/example/retrofit2example/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.retrofit2example;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.util.Log;
 
 import java.util.List;
